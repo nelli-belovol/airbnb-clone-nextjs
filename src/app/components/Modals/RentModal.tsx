d1afcc29fe44fed2d36e44e4a4b5c235bdbeeb93
nelli-belovol <nelli199020@gmail.com>
2023-05-10 16:26:13 +0300

    add description and price

--- src/app/components/Modals/RentModal.tsx
+++ src/app/components/Modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from '../Inputs/CategoryInput';
 import Counter from '../Inputs/Counter';
 import CountrySelect from '../Inputs/CountrySelect';
 import ImageUpload from '../Inputs/ImageUpload';
+import Input from '../Inputs/Input';
 import { categories } from '../Navbar/Categories';
 import Modal from './Modal';
 
@@ -23,7 +24,7 @@ enum STEPS {
 
 const RentModal = () => {
   const rentModal = useRentModal();
-
+  const [isLoading, setIsLoading] = useState(false);
   const [step, setStep] = useState(STEPS.CATEGORY);
 
   const {
@@ -184,6 +185,54 @@ const RentModal = () => {
     );
   }
 
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='How would you describe your place?'
+          subtitle='Short and sweet works best!'
+        />
+        <Input
+          id='title'
+          label='Title'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id='description'
+          label='Description'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Now, set your price'
+          subtitle='How much do you charge per night?'
+        />
+        <Input
+          id='price'
+          label='Price'
+          formatPrice
+          type='number'
+          disabled={isLoading}
+          required
+          errors={errors}
+          register={register}
+        />
+      </div>
+    );
+  }
   return (
     <Modal
       title='Airbnb your home!'
