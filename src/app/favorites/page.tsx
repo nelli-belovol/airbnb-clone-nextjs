fb1424b58f8d13bdcf9aec7cb55f604cae05349a
nelli-belovol <nelli199020@gmail.com>
2023-05-14 20:44:20 +0300

    add favorites

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,29 @@
+import getCurrentUser from '../actions/getCurrentUser';
+import getFavoriteListings from '../actions/getFavoriteListings';
+import ClientOnly from '../components/ClientOnly';
+import EmptyState from '../components/EmptyState';
+import FavoriteClient from './FavoriteClient';
+
+const ListingPage = async () => {
+  const currentUser = await getCurrentUser();
+  const listings = await getFavoriteListings();
+
+  if (!listings.length) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title='No favorites'
+          subtitle='Looks like you have no favorite listings.'
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <FavoriteClient currentUser={currentUser} listings={listings} />
+    </ClientOnly>
+  );
+};
+
+export default ListingPage;
